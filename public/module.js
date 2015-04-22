80cc010a4fe7087cd460aea5afde49feae65daa3
unknown <pmzn560@444150AS201.corp.log.intra.laposte.fr>
2015-04-22 11:46:49 +0200

    AngularJS use with mock led web service

--- public/module.js
+++ public/module.js
@@ -1,8 +1,23 @@
-angular.module('domobyPi', []);
+angular.module('domobyPi', []).value('url', 'http://localhost:9000/json');
 angular.module('domobyPi').config(['$controllerProvider', function ($controllerProvider) {
     $controllerProvider.allowGlobals();
 }]);
 
-function MessageController($scope) {
-    $scope.message = "This is an Angular message.";
-}
+angular.module('domobyPi')
+    .controller('LedController', function ($scope, $http, url) {
+        $scope.led = "off";
+        $scope.isOn = function() {
+            return $scope.led == "on" ;
+        };
+        $scope.ledOn = function () {
+            $http.get(url + '/led/on').success(function (data) {
+                $scope.led = data.led;
+            });
+        };
+        $scope.ledOff = function () {
+            $http.get(url + '/led/off').success(function (data) {
+                $scope.led = data.led;
+            });
+        };
+        
+    });
